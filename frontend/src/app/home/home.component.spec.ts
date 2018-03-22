7dcf1a47404815b59e4667c799bf480b08bef834
agashchuk <agashchuk@corp.local>
2018-03-22 18:48:15 +0300

    first commit

---
+++ frontend/src/app/home/home.component.spec.ts
@@ -0,0 +1,55 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomeComponent } from './home.component';
+import { MockApiService } from '../service/mocks/api.service.mock';
+
+import {
+  MatButtonModule,
+  MatCardModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  ApiService,
+  AuthService,
+  UserService,
+  FooService,
+  ConfigService
+} from '../service';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        HomeComponent
+      ],
+      imports: [
+        MatButtonModule,
+        MatCardModule
+      ],
+      providers: [
+        {
+          provide: ApiService,
+          useClass: MockApiService
+        },
+        AuthService,
+        UserService,
+        FooService,
+        ConfigService
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ server/src/main/java/com/bfwg/config/MainCtr.java
@@ -0,0 +1,12 @@
+package com.bfwg.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainCtr {
+//    @RequestMapping("/login")
+//    public String index() {
+//        return "index";
+//    }
+}
